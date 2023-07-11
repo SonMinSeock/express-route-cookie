d33ad33978aa72d4a7c78076153cc6f21bdbc629
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2023-07-12 08:26:15 +0900

    쿠키 서명하기

--- Cookie_Demo/app.js
+++ Cookie_Demo/app.js
@@ -4,7 +4,7 @@ const cookieParser = require("cookie-parser");
 const app = express();
 const PORT = 3000;
 
-app.use(cookieParser());
+app.use(cookieParser("mysecret"));
 
 app.get("/greet", (req, res) => {
   const { name, animal } = req.cookies;
@@ -16,6 +16,16 @@ app.get("/setname", (req, res) => {
   res.cookie("name", "mike");
   res.cookie("animal", "harlequin shrimp");
   res.send("Ok Send Your Cookie!");
+});
+
+app.get("/getsignedcookie", (req, res) => {
+  res.cookie("fruit", "grape", { signed: true });
+  res.send("Ok Send Your Signed Cookie!");
+});
+
+app.get("/verifyfruit", (req, res) => {
+  console.log(req.signedCookies);
+  res.send(req.signedCookies);
 });
 
 app.listen(PORT, () => console.log(`Server Listen on PORT : ${PORT}`));
